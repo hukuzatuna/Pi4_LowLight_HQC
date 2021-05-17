f50446d4c0e221c7825b7e0a7c73e54dbe71429d
Phil Moyer <hukuzatuna@gmail.com>
2021-05-17 07:07:54 -0700

    Synced with Adafruit guide.

--- lowlight.py
+++ lowlight.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 from picamera import PiCamera
 import time
 from fractions import Fraction
@@ -8,10 +6,10 @@ import datetime
 cur_time = datetime.datetime.now()
 stub = cur_time.strftime("%Y%m%d%H%M_low")
 
-# camera = PiCamera(resolution=(4056,3040),framerate=Fraction(1,6))
 camera = PiCamera(framerate=Fraction(1,6))
 
-# camera.resolution = (3000,1500)
+# You can change these as needed. Six seconds (6000000)
+# is the max for shutter speed and 800 is the max for ISO.
 camera.shutter_speed = 1750000
 camera.iso = 800
 
